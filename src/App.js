8f5def38e2138007a1e24b223ed322aa606433f7
Dreamcatcher96art <dreamcatcher.art96@gmail.com>
2021-07-12 19:51:25 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+        <footer>
+          Coded by Asia Płaska{" "}
+          and {" "}
+          <a
+            href="https://github.com/Dreamcatcher96art/weather-app-react"
+            target="_blank"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
